c053e0c4d8547872eadd16cd5995855c994f3ab6
Luis Quiñones <lpaandres2020@gmail.com>
2025-05-31 08:20:28 -0500

    feat(hyprdrop): build from github:kjlo/hyprdrop

--- pkgs/applications/hyprdrop/default.nix
+++ pkgs/applications/hyprdrop/default.nix
@@ -8,10 +8,10 @@ rustPlatform.buildRustPackage {
   version = "unstable";
 
   src = fetchFromGitHub {
-    owner = "luisnquin";
+    owner = "kjlo";
     repo = "hyprdrop";
-    rev = "fe8f9b11aebeaac114efe4ee45d8a52566fca789";
-    hash = "sha256-meIbPVs+AYTznCeraYi6OO1XfSmkFAJmTBFB5dHUVY8=";
+    rev = "5516c9cde8bd3c5632f09b651dc68bb68743096f";
+    hash = "sha256-g8z9H3aY9LhfkklR98d+cAqjT4yC22BBYfuLdjubhVY=";
   };
 
   nativeBuildInputs = [
@@ -19,9 +19,9 @@ rustPlatform.buildRustPackage {
   ];
 
   cargoLock = {
-    lockFile = ./Cargo.toml;
+    lockFile = ./Cargo.lock;
     outputHashes = {
-      "hyprland-0.4.0-beta.2" = "sha256-Vvpb5BzzTjol6UmJjkqymLlARr4gRJaS5k4aONNlcsk=";
+      "hyprland-0.4.0-beta.2" = "sha256-J7xptA7K9i1kxG4K9+4v2m+Y+XIzNILxlm67S9+FEGc=";
     };
   };
 
