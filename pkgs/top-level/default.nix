bd8cabde1425a91625f9ec36168ad8679e9f67e2
Luis Quiñones <lpaandres2020@gmail.com>
2026-03-21 22:05:59 -0500

    chore: removed unused/already migrated pkgs

--- pkgs/top-level/default.nix
+++ pkgs/top-level/default.nix
@@ -2,22 +2,15 @@
 with lib; let
   packages = {
     coders-crux = ../applications/coders-crux.nix;
-    daktilo = ../applications/daktilo.nix;
     ecsview = ../applications/ecsview.nix;
     emoji-fzf = ../applications/emoji-fzf.nix;
-    encore = ../applications/encore.nix;
     ght = ../applications/ght.nix;
     goweight = ../applications/goweight.nix;
-    havn = ../applications/havn.nix;
     hyprdrop = ../applications/hyprdrop;
-    kmon = ../applications/kmon.nix;
-    logkeys = ../applications/logkeys.nix;
-    minecraft-pumpkin = ../applications/minecraft-pumpkin;
     mysqlweb = ../applications/mysqlweb.nix;
     no = ../applications/no.nix;
     npkill = ../applications/npkill.nix;
     s3-edit = ../applications/s3-edit.nix;
-    systemctl-tui = ../applications/systemctl-tui.nix;
     vimix-cursor-theme = ../applications/vimix-cursor-theme.nix;
   };
 in
