5a04dacd725a48286d5d09b6e0de28fadd783019
Luis Quiñones <lpaandres2020@gmail.com>
2026-05-04 22:18:42 -0500

    feat: centralize it

--- flake.nix
+++ flake.nix
@@ -5,27 +5,83 @@
     nixpkgs.url = "nixpkgs/nixos-unstable";
     systems.url = "github:nix-systems/default";
 
+    adb-mcp = {
+      url = "github:cuentacero/adb-mcp";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        systems.follows = "systems";
+      };
+    };
     nixgrep = {
       url = "github:0xc000022070/nixgrep";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nix-scripts = {
+      url = "github:0xc000022070/nix-scripts";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        systems.follows = "systems";
+      };
+    };
+    passgen = {
+      url = "github:0xc000022070/passgen";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        systems.follows = "systems";
+      };
+    };
+    senv = {
+      url = "github:luisnquin/senv";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    grub-themes = {
+      url = "github:luisnquin/grub-themes";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        systems.follows = "systems";
+      };
+    };
+    hyprstfu = {
+      url = "github:luisnquin/hyprstfu";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        systems.follows = "systems";
+      };
+    };
   };
 
-  outputs = {
+  outputs = inputs @ {
     self,
     nixgrep,
     nixpkgs,
     systems,
+    ...
   }: let
     inherit (nixpkgs) lib;
     eachSystem = lib.genAttrs (import systems);
-  in {
-    overlays.default = final: prev:
+
+    externalOverlays = [
+      inputs.adb-mcp.overlays.default
+      inputs.nix-scripts.overlays.default
+      inputs.passgen.overlays.default
+      inputs.senv.overlays.default
+      inputs.grub-themes.overlays.default
+      inputs.hyprstfu.overlays.default
+    ];
+
+    localOverlay = final: prev:
       import ./pkgs/top-level {
         pkgs = final;
         lib = prev.lib;
         inherit nixgrep;
       };
+  in {
+    overlays.default = lib.composeManyExtensions (
+      externalOverlays
+      ++ [
+        localOverlay
+      ]
+    );
 
     packages = eachSystem (system: let
       pkgs = import nixpkgs {
@@ -35,6 +91,10 @@
           allowBroken = false;
           allowUnfree = true;
         };
+
+        overlays = [
+          self.overlays.default
+        ];
       };
     in
       import ./pkgs/top-level {
