c96f92360d5253cd9cf948c228d0008939c314f9
Luis Quiñones <lpaandres2020@gmail.com>
2026-05-04 20:45:15 -0500

    chore: clean up

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
     systems.url = "github:nix-systems/default";
+
     nixgrep = {
       url = "github:0xc000022070/nixgrep";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -19,21 +20,26 @@
     inherit (nixpkgs) lib;
     eachSystem = lib.genAttrs (import systems);
   in {
-    overlays.default = final: prev: (import ./pkgs/top-level {
-      pkgs = final;
-      inherit nixgrep lib;
-    });
+    overlays.default = final: prev:
+      import ./pkgs/top-level {
+        pkgs = final;
+        lib = prev.lib;
+        inherit nixgrep;
+      };
 
     packages = eachSystem (system: let
       pkgs = import nixpkgs {
+        inherit system;
+
         config = {
           allowBroken = false;
           allowUnfree = true;
         };
-
-        inherit system;
       };
     in
-      import ./pkgs/top-level {inherit pkgs nixgrep;});
+      import ./pkgs/top-level {
+        inherit pkgs nixgrep;
+        lib = pkgs.lib;
+      });
   };
 }
